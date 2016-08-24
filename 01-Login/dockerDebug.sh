2ac2460088520cd4190c65c6c7b18e000cf33a2a
Richard Seldon <arcseldon@gmail.com>
2016-08-24 20:24:28 +0900

    Docker support

--- 01-Login/dockerDebug.sh
+++ 01-Login/dockerDebug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t auth0/auth0-spring-mvc-sample .
+docker build -t auth0/auth0-spring-mvc-sample -f Dockerfile .
 docker build -t auth0/auth0-spring-mvc-sample-debug -f Dockerfile-debug .
 docker run -it -p 3099:3099 -p 8000:8000 \
        -v $(pwd):/usr/src/app \
