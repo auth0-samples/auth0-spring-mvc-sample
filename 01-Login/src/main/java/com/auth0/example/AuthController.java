5ae13b73650b77bdb8216abc23a9fdafd2825842
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-08-31 15:06:07 -0300

    request userinfo audience

--- 01-Login/src/main/java/com/auth0/example/AuthController.java
+++ 01-Login/src/main/java/com/auth0/example/AuthController.java
@@ -7,17 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
 
 @Component
 public class AuthController {
 
     private final AuthenticationController controller;
+    private final String userInfoAudience;
 
     @Autowired
     public AuthController(AppConfig config) {
         controller = AuthenticationController.newBuilder(config.getDomain(), config.getClientId(), config.getClientSecret())
                 .build();
+        userInfoAudience = String.format("https://%s/userinfo", config.getDomain());
     }
 
     public Tokens handle(HttpServletRequest request) throws IdentityVerificationException {
@@ -25,7 +26,9 @@ public class AuthController {
     }
 
     public String buildAuthorizeUrl(HttpServletRequest request, String redirectUri) {
-        return controller.buildAuthorizeUrl(request, redirectUri)
+        return controller
+                .buildAuthorizeUrl(request, redirectUri)
+                .withAudience(userInfoAudience)
                 .build();
     }
 
