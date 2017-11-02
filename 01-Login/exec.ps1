4bd152d6dad771d63da61a076e1b27f9d34ee637
Alexis Luque <aluque@indexlabs.com.uy>
2017-11-02 15:31:50 -0300

    Add docker

---
+++ 01-Login/exec.ps1
@@ -0,0 +1,2 @@
+docker build -t auth0-spring-mvc-webapp .
+docker run -p 8080:8080 -it auth0-spring-mvc-webapp
---
+++ 01-Login/exec.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build -t auth0-spring-mvc-webapp .
+docker run -p 8080:8080 -it auth0-spring-mvc-webapp
