2c38230e387e1dca9c61e50e96f714c1c03b5082
Alexis Luque <aluque@indexlabs.com.uy>
2018-02-07 13:42:45 -0300

    Update docker tag name

--- 01-Login/exec.ps1
+++ 01-Login/exec.ps1
@@ -1,2 +1,2 @@
-docker build -t auth0-samples/spring-mvc-00login:latest .
-docker run -p 3000:3000 -v ~/.m2:/home/gradle/.gradle -it auth0-samples/spring-mvc-00login:latest
+docker build -t auth0-spring-mvc-01-login .
+docker run -p 3000:3000 -v ~/.m2:/home/gradle/.gradle -it auth0-spring-mvc-01-login
