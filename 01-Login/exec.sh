15231339e5619a037c5f0ae5df41538a901798b1
Alexis Luque <aluque@indexlabs.com.uy>
2018-02-07 17:31:58 -0300

    Remove volume mouting from scripts

--- 01-Login/exec.sh
+++ 01-Login/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t auth0-samples/auth0-spring-mvc-01-login .
-docker run -p 3000:3000 -v ~/.m2:/home/gradle/.gradle -it auth0-samples/auth0-spring-mvc-01-login
+docker run -p 3000:3000 -it auth0-samples/auth0-spring-mvc-01-login
