021185ca343e1971e9cdacbb9ff2dd90c49692a0
Alexis Luque <aluque@indexlabs.com.uy>
2018-04-17 16:08:38 -0300

    Logout from Auth0

--- 01-Login/src/main/java/com/auth0/example/LogoutController.java
+++ 01-Login/src/main/java/com/auth0/example/LogoutController.java
@@ -13,12 +13,21 @@ import javax.servlet.http.HttpServletRequest;
 public class LogoutController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final String domain;
+    private final String clientId;
+
+    public LogoutController(AppConfig config){
+        domain = config.getDomain();
+        clientId = config.getClientId();
+    }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     protected String logout(final HttpServletRequest req) {
         logger.debug("Performing logout");
         invalidateSession(req);
-        return "redirect:" + req.getContextPath() + "/login";
+        String returnTo = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort();
+        String logoutUrl = String.format("https://%s/v2/logout?client_id=%s&returnTo=%s", domain, clientId, returnTo);
+        return "redirect:" + logoutUrl;
     }
 
     private void invalidateSession(HttpServletRequest request) {
